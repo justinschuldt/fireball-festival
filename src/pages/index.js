e2b0587ba459b5cc8a25dc4859078a8e28aa4fc7
justinschuldt <justinschuldt@gmail.com>
2024-10-29 10:03:52 -0500

    add 2025 fundraiser info

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,25 @@ import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
 import Gallery from 'components/gallery';
+import Item from 'components/gallery/item'
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import MEDIA from 'helpers/mediaTemplates';
+
+export const FeatureContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 4rem;
+  padding: 0 4rem;
+  margin: 2rem 0;
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+
+  ${MEDIA.TABLET`
+    display: block;
+  `};
+`;
 
 const Index = ({ data }) => (
   <Layout>
@@ -21,6 +39,15 @@ const Index = ({ data }) => (
         }}
       />
     </Box>
+
+    {data.homeJson.featureGallery && data.homeJson.featureGallery.length == 1 ?
+
+
+      <FeatureContainer>
+        <Item {...data.homeJson.featureGallery[0]}></Item>
+      </FeatureContainer>
+      : null}
+
   </Layout>
 );
 
@@ -47,6 +74,17 @@ export const query = graphql`
         }
       }
       gallery {
+        title
+        copy
+        image {
+          childImageSharp {
+            fluid(maxHeight: 500, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      featureGallery {
         title
         copy
         image {
